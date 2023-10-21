cbbb93df92a0987c09ecc330304b7cd692b0b4c0
unknown <lizzybumpers11@gmail.com>
2023-10-21 14:32:44 -0500

    uh tried adding things into localStorage (didnt go well)

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,21 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-function updateClock() {
-  const today = dayjs().format("ddd MMM MM, YYYY, hh:mm:ss");
-  $("#currentDay").text(today);
-}
-setInterval(updateClock, 1000)
-
 $(function module() {
+  let plan = document.getElementsByClassName("description")[0].value;
+  let submitBtn = document.querySelector(".saveBtn");
+  submitBtn.addEventListener("click", function () {
+  
+  });
+
+
+  function updateClock() {
+    const today = dayjs().format("ddd MMM D, YYYY, hh:mm:ss");
+    $("#currentDay").text(today);
+  }
+  // setInterval(updateClock, 1000)
+});
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -26,4 +34,3 @@ $(function module() {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
